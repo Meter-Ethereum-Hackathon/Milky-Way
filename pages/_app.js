bcf2aac192f8d754f0ae8adcc2e5aae9e42af457
Parthivi Varshney <varshney.pa@northeastern.edu>
2022-04-07 14:39:23 -0700

    Added Individual Card Page, Chnaged card design on marketplace and my nfts

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,8 @@ import logo2 from "../public/logo2.png";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-gradient-to-b from-slate-900 via-slate-900 to-blue-900 h-full w-full ">
-      <nav className="flex sticky top-0 items-center justify-between flex-wrap shadow-lg bg-gradient-to-r from-slate-900 via-indigo-800 to-slate-900 p-6  shadow-blue-500/50 ">
+    <div className="bg-slate-900  h-90 overflow-hidden">
+      <nav className="flex sticky-top-0 items-center justify-between flex-wrap shadow-lg bg-gradient-to-r from-slate-900 via-indigo-800 to-slate-900 p-6  shadow-blue-500/50 ">
         <div className="flex items-center flex-shrink-0 text-white mr-6 ">
           <Image
             src={logo2}
@@ -30,24 +30,24 @@ function MyApp({ Component, pageProps }) {
             <Link href="/">
               <a
                 href="#responsive-header"
-                className="block pr-8 mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-purple-600 mr-4"
+                className="block pr-8 mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-purple-600 mr-4 hover:animate-pulse"
               >
                 MARKETPLACE
               </a>
             </Link>
-            <Link href="/my-nft">
+            {/* <Link href="/my-nft">
               <a
                 href="#responsive-header"
                 className="block pr-8 mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-purple-600"
-              >
+              > */}
                 {/* view own digital assets (collections) */}
-                MY NFT
-              </a>
-            </Link>
+                {/* MY NFT */}
+              {/* </a>
+            </Link> */}
             <Link href="/dashboard">
               <a
                 href="#responsive-header"
-                className="block pr-12 mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-purple-600"
+                className="block pr-12 mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-purple-600 hover:animate-pulse"
               >
                 {/* view creator's own NFTs, shows what creator's created and sold */}
                 DASHBOARD
@@ -60,14 +60,16 @@ function MyApp({ Component, pageProps }) {
                 href="#"
                 className="inline-block  text-l px-5 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-fuchsia-500 hover:bg-white/50 mt-4 lg:mt-0 shadow-lg shadow-indigo-400/60"
               >
-                SELL
+                CREATE
               </a>
             </Link>
           </div>
         </div>
       </nav>
-
-      <Component {...pageProps} />
+    <div className="app-body p-10">
+    <Component {...pageProps} />
+    </div>
+     
     </div>
   );
 }
